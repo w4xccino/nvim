9c1abf97e1bb7673800e442c0e28d4294b533f12
Diego Toledo <dbtoledoa@gmail.com>
2024-08-07 23:41:40 -0500

    test fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,9 @@
 return {
 	'tpope/vim-fugitive',
+	cmd = {"G", "Git"},
+	lazy = true,
+	keys = {
+		{"<leader>gg", ":Git <cr>", desc = "Git Menu"},
+		{"<leader>gp", ":Git pull <cr>", desc = "Git Pull"},
+	},
 }
