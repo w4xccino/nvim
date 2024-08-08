dec51d8c489b5293bb2199b994923f4e56e3548d
Diego Toledo <diego.toledo@apselom.com>
2024-08-08 15:09:04 -0500

    add push to fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,6 +4,7 @@ return {
 	lazy = true,
 	keys = {
 		{"<leader>gg", ":Git <cr>", desc = "Git Menu"},
-		{"<leader>gp", ":Git pull <cr>", desc = "Git Pull"},
+		{"<leader>gl", ":Git pull <cr>", desc = "Git Pull"},
+		{"<leader>gp", ":Git push origin HEAD <cr>", desc = "Git Push"},
 	},
 }
