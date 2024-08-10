401fe2826867eb2aa858c3e233621f2b4da736a2
Diego Toledo <diego.toledo@apselom.com>
2024-08-09 22:27:35 -0500

    feat: add mason

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,26 @@
+return {
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    'williamboman/mason.nvim',
+    'folke/neodev.nvim',
+  },
+  config = function()
+    local on_attach = function (_, bufnr)
+      vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+      local opts = {buffer = bufnr}
+
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    end
+    require('neodev').setup()
+    require('lspconfig').lua_ls.setup({
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          telemetry = { enable = false},
+          workspace = { checkThirdParty = false },
+        }
+      }
+    })
+  end
+}
